e1c233adc9af4f0b7d36b8eb03f70b12517781c7
Elisa <tun25675@temple.edu>
2024-01-19 16:11:34 -0500

    Fixed random

--- random.c
+++ random.c
@@ -2,4 +2,10 @@
 //create randchar() to return random char
 //link random.c to print_random.c
 
+#include <stdlib.h>
+#include <time.h>
 
+char randchar() {
+    srand(time(NULL));
+    return 'A' + (rand() % 26);
+}
